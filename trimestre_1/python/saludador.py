0258e4682c8ebd7ce2b07e148db085b2b966835e
juanpesttom <59234485+juanpesttom@users.noreply.github.com>
2021-09-22 11:44:53 +0200

    Create saludador.py

---
+++ trimestre_1/python/saludador.py
@@ -0,0 +1,8 @@
+def saludador():
+    nombre = raw_input("Cual es tu nombre")
+    edad = raw_input("Cuantos años tienes")
+    lugar = raw_input("Donde vives")
+    print "Buenos dias", nombre
+    print edad, lugar
+    
+saludador()
