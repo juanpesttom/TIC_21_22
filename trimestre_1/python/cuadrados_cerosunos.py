b976ae4eb9dab3ef8de98f6dd3097aec1bbc69ce
juanpesttom <59234485+juanpesttom@users.noreply.github.com>
2021-11-25 08:57:43 +0100

    Update cuadrados_cerosunos.py

--- trimestre_1/python/cuadrados_cerosunos.py
+++ trimestre_1/python/cuadrados_cerosunos.py
@@ -1,50 +1,29 @@
-"""
-111
-101
-111
-
-11111
-10001
-10101
-10001
-11111
-
-1111111
-1000001 
-1011101 
-1010101 
-1011101
-1000001
-1111111
-"""
-
-def swap(c1, c2):
-    tmp = c1
-    c1 = c2
-    c2 = tmp
-    
-def main():
-    lenght = 5
+def cuadrado_2():
+    lenght = input("Introduce la longitud: ")
     l = [[" " for i in range(lenght)] for i in range(lenght)]
     
     n1 = 0
-    n2 = lenght
-    c1 = '1'
-    c2 = '0'
+    n2 = lenght-1
+    c = '1'
     
     for i in range(lenght):
-        for j in range(lenght):
-            if j > n1 or j < n2:
-                l[i][j] = c2
-            else:
-                l[i][j] = c1
+        for j in range(n1, n2+1):
+            l[n1][j] = c
+            l[n2][j] = c
+            l[j][n1] = c
+            l[j][n2] = c
+        if c == '1':
+            c = '0'
+        else:
+            c = '1'
         n1 += 1
-        n2 -= 1
+        n2 -=1
 
     for i in l:
         for j in i:
             print j,
         print 
+def main():
         
     
     
