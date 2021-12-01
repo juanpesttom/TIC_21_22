93a5c1d5315786002c01790e8700dea98db1c9c0
juanpesttom <59234485+juanpesttom@users.noreply.github.com>
2021-12-01 12:01:47 +0100

    Create primer_programa.cpp

---
+++ trimestre_1/C++/primer_programa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int salir;
+    
+    printf("Hola mundo\n");
+    printf("Presiona cualquier tecla para salir: ");
+    scanf("%c", &salir);
+
+    return 0;
+}
