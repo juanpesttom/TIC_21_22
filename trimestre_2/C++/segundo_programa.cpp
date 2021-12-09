511573164df0ac560efb27de5c4253ff4ea4d352
juanpesttom <59234485+juanpesttom@users.noreply.github.com>
2021-12-09 08:54:36 +0100

    Create segundo_programa.cpp

---
+++ trimestre_2/C++/segundo_programa.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[10];
+    int i;
+    
+    for (i = 0; i < 10; i++)
+    {
+        printf("Dame un numero: ");
+        scanf("%d", &arr[i]);
+    }
+}
